5c1b99b90ae7a1c99d674142ed7c13aa1847686b
chuyuehlin <ggn0410gg@gmail.com>
2021-05-27 20:15:12 +0800

    v.2

--- stock_crawler/dags/fugle/fugle_crawler_chart.py
+++ stock_crawler/dags/fugle/fugle_crawler_chart.py
@@ -1,22 +1,32 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
+import time
 import requests
 from elasticsearch import Elasticsearch
+import json
 
 from airflow import DAG
 from airflow.operators.http_operator import SimpleHttpOperator
 from airflow.operators.python_operator import PythonOperator
 default_args = {
     "owner": "Josix",
-    "start_date": datetime(2021, 5, 1, 0, 0),
-	"schedule_interval"='*/1 * * * *', #..every 1 minutes
+    "start_date": datetime(2021, 5, 24, 0, 0),
+	"schedule_unterval":'* 9-14 * 1-5 *', #every 1 minutes #MON to FRI
     "retries": 3,
     "retry_delay": timedelta(minutes=5),
 }
 
 def get_chart_func(no,ti):
+	#set current date and time
+	local = time.localtime()
+	today = date.today()
+	current = today.strftime("%Y-%m-%dT")+time.strftime("%H:%M", local)+":00.000+08:00"
+
 	r = requests.get('https://api.fugle.tw/realtime/v0.2/intraday/chart?symbolId=') + no + ('&apiToken=706707e3df7e8e54a6932b59c85b77ca')
 	r = r.json()
 	r = r['data']['chart']
+	r = r[current]#get rid off noncurrent data
+	r.update({"date":today.strftime("%Y-%m-%d"),"time":time.strftime("%H:%M", local),"stock":no})
+	r = json.dump(r)
 	ti.xcom_push(key='chart_'+no, value=r)
 
 def post_chart_ES_func(no,ti):
