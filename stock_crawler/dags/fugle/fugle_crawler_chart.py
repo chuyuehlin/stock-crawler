0c1197ab43f6024ae158f1c3873eb322bdfabc7e
chuyuehlin <ggn0410gg@gmail.com>
2021-05-29 00:18:33 +0800

    debug

--- stock_crawler/dags/fugle/fugle_crawler_chart.py
+++ stock_crawler/dags/fugle/fugle_crawler_chart.py
@@ -1,53 +1,63 @@
 from datetime import datetime, timedelta, date
+import pendulum
 import time
 import requests
 from elasticsearch import Elasticsearch
 import json
+import os
 
 from airflow import DAG
-from airflow.operators.http_operator import SimpleHttpOperator
-from airflow.operators.python_operator import PythonOperator
-default_args = {
-    "owner": "Josix",
-    "start_date": datetime(2021, 5, 24, 0, 0),
-	"schedule_unterval":'* 9-14 * 1-5 *', #every 1 minutes #MON to FRI
-    "retries": 3,
-    "retry_delay": timedelta(minutes=5),
+from airflow.operators.python_operator import PythonOperator, PythonVirtualenvOperator
+from airflow.utils.db import provide_session
+from airflow.models import XCom
+def cleanup_xcom(context, session=None):
+	dag_id = context['ti']['dag_id']
+	session.query(XCom).filter(XCom.dag_id == dag_id).delete()
+
+local_tz = pendulum.timezone("Asia/Taipei")
+args = {
+	"owner":"Josix",
+	"retries":3,
+	"retry_delay":timedelta(seconds=10),
 }
 
-def get_chart_func(no,ti):
-	#set current date and time
+def get_chart_func(r,no,ti):
+	time_now = time.strftime("%H:%M", local)
+	if time_now = '09:00' or (time_now > '13:26' and time_now != '13:31'):
+		continue
+	#from 9:01 to 13:26 and 13:31
 	local = time.localtime()
 	today = date.today()
-	current = today.strftime("%Y-%m-%dT")+time.strftime("%H:%M", local)+":00.000+08:00"
-
-	r = requests.get('https://api.fugle.tw/realtime/v0.2/intraday/chart?symbolId=') + no + ('&apiToken=706707e3df7e8e54a6932b59c85b77ca')
+	current = today.strftime("%Y-%m-%dT")+time.strftime("%H:", local)+"{:02d}".format(local.tm_min-1)+":00.000+08:00"
+	#data will delay 1 min
 	r = r.json()
 	r = r['data']['chart']
-	r = r[current]#get rid off noncurrent data
-	r.update({"date":today.strftime("%Y-%m-%d"),"time":time.strftime("%H:%M", local),"stock":no})
-	r = json.dump(r)
+	r = r[current]
+	r.update({"date":today.strftime("%Y-%m-%d"),"time":time.strftime("%H:", local)+"{:02d}".format(local.tm_min-1),"stock":no})	
 	ti.xcom_push(key='chart_'+no, value=r)
 
-def post_chart_ES_func(no,ti):
+def post_chart_ES_func(no,ti):	
 	es = Elasticsearch(hosts='127.0.0.1', port=9200)
 	data = ti.xcom_pull(key='chart_'+no)
+	data = json.dumps(data)
 	es.index(index='chart', body=data)
 
-dag_chart = DAG(
+with DAG(
     'fugle_chart',
-    default_args=default_args,
-    description='Fugle Chart API DAG',
+	default_args=args,
+	start_date=datetime(2021, 5, 25, tzinfo=local_tz),
+	schedule_interval="*/1 9-14 * * 1-5",#MON to FRI midnight in morning
+    description='Fugle Meta API DAG',
     tags=['chart', 'fugle'],
-)
-
-with dag_chart:
-	stocks = ['2330','2454']
+	on_success_callback=cleanup_xcom
+) as dag_chart:
+	stocks = ['2330']
 	for stock in stocks:
+		r = requests.get('https://api.fugle.tw/realtime/v0.2/intraday/chart?symbolId='+stock+'&apiToken=706707e3df7e8e54a6932b59c85b77ca')
 		get_chart = PythonOperator(
 			task_id='get_chart_'+stock,
 			python_callable=get_chart_func,
-			op_kwargs={'no': stock}
+			op_kwargs={'r':r,'no':stock}
 		)
 		post_chart_ES = PythonOperator(
 			task_id='post_chart_ES_'+stock,
--- stock_crawler/dags/fugle/fugle_crawler_meta.py
+++ stock_crawler/dags/fugle/fugle_crawler_meta.py
@@ -7,26 +7,18 @@ import json
 import os
 
 from airflow import DAG
-from airflow.providers.http.operators.http import SimpleHttpOperator
-from airflow.providers.http.sensors.http import HttpSensor
-from airflow.operators.python_operator import PythonOperator, PythonVirtualenvOperator
-from airflow.operators.bash import BashOperator
-from airflow.operators.dummy import DummyOperator
+from airflow.operators.python_operator import PythonOperator
 from airflow.utils.db import provide_session
 from airflow.models import XCom
-def cleanup_xcom(context, session=None):
-	dag_id = context['ti']['dag_id']
-	session.query(XCom).filter(XCom.dag_id == dag_id).delete()
 
 local_tz = pendulum.timezone("Asia/Taipei")
 args = {
 	"owner":"Josix",
-	#"retries":3,
-	#"retry_delay":timedelta(seconds=10),
+	"retries":3,
+	"retry_delay":timedelta(seconds=10),
 }
 
 def get_meta_func(r,no,ti):
-	#r = requests.get('https://api.fugle.tw/realtime/v0.2/intraday/meta?symbolId=2330&apiToken=706707e3df7e8e54a6932b59c85b77ca')
 	r = r.json()
 	r = r['data']['meta']
 	pop_attributes = ["isIndex","nameZhTw","industryZhTw","canDayBuySell","canDaySellBuy","canShortMargin","canShortLend","volumePerUnit","currency","isWarrant","typeZhTw","isUnusuallyRecommended"]
@@ -41,38 +33,27 @@ def post_meta_ES_func(no,ti):
 	es = Elasticsearch(hosts='127.0.0.1', port=9200)
 	data = ti.xcom_pull(key='meta_'+no)
 	data = json.dumps(data)
-	#requests.post('127.0.0.1:9200',headers={'Content-Type':'application/json'},data = data)
 	es.index(index='meta', body=data)
 
 with DAG(
     'fugle_meta',
 	default_args=args,
 	start_date=datetime(2021, 5, 25, tzinfo=local_tz),
-	schedule_interval="* * * * 1-5",#MON to FRI midnight in morning
+	schedule_interval="5 0 * * 1-5",#MON to FRI 00:05 in morning
     description='Fugle Meta API DAG',
     tags=['meta', 'fugle'],
-	on_success_callback=cleanup_xcom
 ) as dag_meta:
-	stocks = ['2330']
+	stocks = ['2330','2454']
 	for stock in stocks:
-		r = requests.get('https://api.fugle.tw/realtime/v0.2/intraday/meta?symbolId=2330&apiToken=706707e3df7e8e54a6932b59c85b77ca')
+		r = requests.get('https://api.fugle.tw/realtime/v0.2/intraday/meta?symbolId='+stock+'&apiToken=706707e3df7e8e54a6932b59c85b77ca')
 		get_meta = PythonOperator(
 			task_id='get_meta_'+stock,
 			python_callable=get_meta_func,
-			op_kwargs={'r': r,'no':stock}
+			op_kwargs={'r':r,'no':stock}
 		)
 		post_meta_ES = PythonOperator(
-				task_id='post_meta_ES_'+stock,
+			task_id='post_meta_ES_'+stock,
 			python_callable=post_meta_ES_func,
 			op_kwargs={'no': stock}
-			)
-		'''
-		post_meta_ES = SimpleHttpOperator(
-			task_id='post_op',
-			http_conn_id='127.0.0.1:9200',
-			endpoint='meta/_doc',
-			data=json.dumps("{{task_instance.xcom_pull(key='meta_'+stock)}}"),
-			headers={"Content-Type":"application/json"},
 		)
-		'''
 		get_meta >> post_meta_ES
