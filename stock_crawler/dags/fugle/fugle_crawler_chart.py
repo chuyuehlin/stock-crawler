058e83815d3fb903029bb4678f3a7aca938920ab
chuyuehlin <ggn0410gg@gmail.com>
2021-06-01 22:38:15 +0800

    debug

--- stock_crawler/dags/fugle/fugle_crawler_chart.py
+++ stock_crawler/dags/fugle/fugle_crawler_chart.py
@@ -19,20 +19,20 @@ args = {
 }
 
 def get_chart_func(r,no,ti):
-        r = r.json()
-        r = r['data']['chart']
-        time_list = [ t for t in r]
-        push_items = []
-        for time in time_list:
-                item = r[time]
-                item.update({"time":time,"stock":no})
-                push_items.append(item) 
-        ti.xcom_push(key='chart_'+no, value=push_items)
+	r = r.json()#request from outside
+	r = r['data']['chart']#data for every minute 
+	time_list = [ t for t in r]#build list of time(string) from 9:00 to 13:30
+	push_items = []
+	for time in time_list:
+		item = r[time] #find out the data in the list for every time
+		item.update({"time":time,"stock":no}) #record time into the data
+		push_items.append(item) #save it 
+	ti.xcom_push(key='chart_'+no, value=push_items) #push it the xcom
 
 def post_chart_ES_func(no,ti):
         es = Elasticsearch(hosts='127.0.0.1', port=9200)
         datas = ti.xcom_pull(key='chart_'+no)
-        for data in datas:
+        for data in datas:#insert each of data into ES
                 data = json.dumps(data) 
                 es.index(index='chart', body=data)
 
@@ -40,11 +40,11 @@ with DAG(
 	'fugle_chart',
 	default_args=args,
 	start_date=datetime(2021, 5, 27, tzinfo=local_tz),
-	schedule_interval="*/1 9-14 * * 1-5",#MON to FRI every minute from 9:00 to 14:00
+	schedule_interval="0 17 * * 1-5",#MON to FRI 17:00
 	description='Fugle Meta API DAG',
 	tags=['chart', 'fugle'],
 ) as dag_chart:
-	stocks=['2330']
+	stocks=['1101', '1102', '1103', '1104', '1109','2330']
 	for stock in stocks:
 		r = requests.get('https://api.fugle.tw/realtime/v0.2/intraday/chart?symbolId='+stock+'&apiToken=706707e3df7e8e54a6932b59c85b77ca')
 		get_chart = PythonOperator(
